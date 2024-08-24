396d67a7f66191875c174faa8be5a3c611eabd27
iSAD <bradx2713@gmail.com>
2024-08-24 23:41:25 +0200

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,43 @@ void main() {
     debugShowCheckedModeBanner: false,
     home: Scaffold(
       backgroundColor: Colors.lime,
-      body: Center(),
+      body: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(32),
+        decoration: const BoxDecoration(
+          image:
+              DecorationImage(image: AssetImage("images/background-plain.jpg")),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [Image(image: AssetImage("images/logo.png")),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [Image(image: AssetImage("images/back.png")), Image(image: AssetImage("images/back.png"))],
+          ),
+            Image(image: AssetImage("images/button.png")),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("PLAYER"),
+                    Text("0")
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text("CPU"),
+                    Text("0")
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     ),
   ));
 }
