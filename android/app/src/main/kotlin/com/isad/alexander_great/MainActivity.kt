28e7f69f64b23a6abece7d86579530df001b568a
iSAD <bradx2713@gmail.com>
2024-08-24 23:25:46 +0200

    Updated

---
+++ android/app/src/main/kotlin/com/isad/alexander_great/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.isad.alexander_great
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.lime,
+      body: Center(),
+    ),
+  ));
+}
